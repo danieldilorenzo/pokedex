1377102d6009d0a9d4a3088299ba801a1e0a8b0a
Daniel Di Lorenzo <danieldilorenzoferreira@gmail.com>
2021-11-16 21:49:21 -0300

    16-11

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function show(
+function nome() {
+  const temp = [
+    {
+      id: 1,
+      name: "Xablau",
+    },
+    {
+      id: 2,
+      name: "Fernando",
+    },
+    {
+      id: 3,
+      name: "Juliano",
+    },
+  ];
+  for (let name = 0; name < temp.length; name++) {
+    //console.log(name);
+    console.log(temp[name]);
+  }
+}
 
-    
-)
+nome();
